fe8a76f2f597043316abedf7bcb17db3a6ac273a
erizou <oshikuma3@gmail.com>
2025-06-16 15:56:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from flask import Flask, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
 @app.route("/scenario")
 def get_scenario():
-    with open("scenario.json", encoding="utf-8") as f:
+    return jsonify({
+        "title": "HERO☆ハイスクール",
+        "genre": ["アクション", "学園", "SF"]
+    })
+
+@app.route("/characters")
+def get_characters():
+    with open("character.json", encoding="utf-8") as f:
         data = json.load(f)
     return jsonify(data)
 
-@app.route("/characotor")
-def get_scenario():
-    with open("characotor.json", encoding="utf-8") as f:
-        data = json.load(f)
-    return jsonify(data)
-
-import os
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+# ポート設定（Render向け）
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
