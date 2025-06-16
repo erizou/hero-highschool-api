ce3968b37b5054b401c0223ac2757190522a5b6f
erizou <oshikuma3@gmail.com>
2025-06-16 14:51:36 +0900

    Update master.py

--- master.py
+++ master.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+import json
+
 @app.route("/scenario")
 def get_scenario():
-    return jsonify({
-        "title": "HERO☆ハイスクール",
-        "genre": ["アクション", "学園", "SF"]
-    })
-
+    with open("scenario.json", encoding="utf-8") as f:
+        data = json.load(f)
+    return jsonify(data)
 
 import os
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
