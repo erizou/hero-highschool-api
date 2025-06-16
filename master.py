ef907b3af6e060abf482333a9db701098164a341
erizou <oshikuma3@gmail.com>
2025-06-16 13:37:57 +0900

    Update master.py

--- master.py
+++ master.py
@@ -2,11 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route("/scenario")
+def get_scenario():
     return jsonify({
-        "message": "HERO☆ハイスクールAPIへようこそ！"
+        "title": "HERO☆ハイスクール",
+        "genre": ["アクション", "学園", "SF"]
     })
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
