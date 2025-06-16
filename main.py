22a06b93db79f5ce42c665a900a032cd20361743
erizou <oshikuma3@gmail.com>
2025-06-16 13:48:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ def get_scenario():
         "genre": ["アクション", "学園", "SF"]
     })
 
-if __name__ == "__main__":
-    app.run()
+
+import os
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
